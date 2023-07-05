3043b956a465e81b1f77ab16de45385375cf32e9
kane-e <68760965+kane-e@users.noreply.github.com>
2023-07-04 21:59:58 -0700

    updated print outputs

--- trips.py
+++ trips.py
@@ -4,19 +4,19 @@ import csv
 import io
 
 COLOR_RED   = "\033[1;31m"
-COLOR_BLUE  = "\033[1;34m"
 COLOR_GREEN = "\033[0;32m"
 COLOR_RESET = "\033[0;0m"
 
 def convert_trip_names():
-    filepath = sys.argv[1]
     if len(sys.argv) < 2:
-        print(COLOR_RED + "No filename provided!" + COLOR_RESET)
+        print(COLOR_RED + "No file provided!" + COLOR_RESET)
         exit()
+    filepath = sys.argv[1]
     if not os.path.isfile(filepath):
-        print(COLOR_RED + "File does not exist!" + COLOR_RESET)
-    if not os.path.basename(filepath) == 'trips.txt':
-        print(COLOR_RED + "Incorrect file input! Please input trips.txt" + COLOR_RESET)
+        print(COLOR_RED + "Input is not a file! Please input trips.txt." + COLOR_RESET)
+        exit()
+    if not os.path.basename(filepath) == "trips.txt":
+        print(COLOR_RED + "Incorrect file input! Please input trips.txt." + COLOR_RESET)
         exit()
     make_new_file(filepath)
 
@@ -26,12 +26,14 @@ def make_new_file(filepath):
         csv_file = csv.DictReader(trips_txt)
         csv_list = list(csv_file)
         for row in csv_list:
-            if "trip_short_name" not in row or not row["trip_short_name"]:
-                print(COLOR_BLUE + "no trip_short_name values are present and therefore cannot be deleted" + COLOR_RESET)
+            if "trip_short_name" not in row: 
+                print(COLOR_RED + "No trip_short_name column detected. The operation cannot be performed. Review the file and try again." + COLOR_RESET)
                 return
-        file_name = 'trips2.txt'
+            if not row["trip_short_name"]:
+                print(COLOR_RED + "Missing trips_short_name value(s). Operation was completed, but ensure this is expected before proceeding." + COLOR_RESET)
+        file_name = "trips2.txt"
         if os.path.exists(file_name):
-            print(COLOR_RED + "File with name " + file_name + " already exists in directory; cannot create a new one. Move this file and try again. Skipping..." + COLOR_RESET)
+            print(COLOR_RED + "File with name " + file_name + " already exists in directory; cannot create a new one. Move this file and try again." + COLOR_RESET)
             return
         new_file = open(file_name, "w")
         new_csv_writer = csv.DictWriter(new_file, fieldnames=csv_file.fieldnames)
@@ -40,7 +42,7 @@ def make_new_file(filepath):
             row["trip_short_name"] = ""
             new_csv_writer.writerow(row)
         new_file.close()
-        print(COLOR_GREEN + "Exported " + file_name + " with trip short names removed" + COLOR_RESET)
+        print(COLOR_GREEN + "Exported " + file_name + " with trip short names removed." + COLOR_RESET)
 
 if __name__ == "__main__":
     convert_trip_names()
